30942bdee2b6d9ce4a2d756b84137d522d4de435
Sebastiano Peluso <peluso@gsd.inesc-id.pt>
2013-02-02 00:11:09 +0100

    Copyright and headers

--- src/eu/cloudtm/wpm/consumer/SenderConsumer.java
+++ src/eu/cloudtm/wpm/consumer/SenderConsumer.java
@@ -1,3 +1,26 @@
+/*
+ * CINI, Consorzio Interuniversitario Nazionale per l'Informatica
+ * Copyright 2013 CINI and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3.0 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+ 
 package eu.cloudtm.wpm.consumer;
 
 import java.io.BufferedInputStream;
@@ -11,6 +34,9 @@ import java.net.UnknownHostException;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
+/*
+* @author Roberto Palmieri
+*/
 public class SenderConsumer implements Runnable{
 	private String logService_addr;
 	private int logService_port;
